a5d0ff81e988be64327c0e57580d83f951c783b9
Suyash Negi <negisuyash1998@gmail.com>
2020-04-17 18:12:47 +0530

    Spring start initial

---
+++ owta/src/main/java/com/OWTA/owta/OwtaApplication.java
@@ -0,0 +1,13 @@
+package com.OWTA.owta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwtaApplication.class, args);
+	}
+
+}
